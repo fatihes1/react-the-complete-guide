8aab3a6b2a512b51999fcf3f6da6c0014284b58f
Fatih Es <18360859021@ogrenci.btu.edu.tr>
2023-02-02 20:17:25 +0300

    complated section-3

---
+++ codes/01-starting-setup/src/components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.data[0].title}
+        amount={props.data[0].amount}
+        date={props.data[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.data[1].title}
+        amount={props.data[1].amount}
+        date={props.data[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.data[2].title}
+        amount={props.data[2].amount}
+        date={props.data[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.data[3].title}
+        amount={props.data[3].amount}
+        date={props.data[3].date}
+      ></ExpenseItem>
+    </Card>
+  );
+}
+
+export default Expenses;
