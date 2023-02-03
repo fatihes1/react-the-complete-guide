44a3a4a2508c3deecbc366d9680ee093667469f0
Fatih Es <18360859021@ogrenci.btu.edu.tr>
2023-02-03 14:07:44 +0300

    NewExense | ExpenseForm components

--- codes/01-starting-setup/src/App.js
+++ codes/01-starting-setup/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
   const expenses = [
     {
@@ -25,7 +26,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses data={expenses} />
     </div>
   );
---
+++ codes/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  const titleChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amont</label>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="2019-01-01" max="2022-12-31" />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
