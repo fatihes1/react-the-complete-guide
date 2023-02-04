af2d11e79a49f3a899037024c6c1c4dea84cffbb
Fatih Es <18360859021@ogrenci.btu.edu.tr>
2023-02-04 16:57:49 +0300

    small bug in added expense

--- codes/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ codes/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -54,7 +54,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
