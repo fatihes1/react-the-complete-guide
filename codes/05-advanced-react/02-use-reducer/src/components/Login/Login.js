92a88b738b2a92a45228dce455f4a0055c5894d3
Fatih Es <18360859021@ogrenci.btu.edu.tr>
2023-02-09 16:58:38 +0300

    React Context API | provides | consumer | useContext hook

--- codes/05-advanced-react/02-use-reducer/src/components/Login/Login.js
+++ codes/05-advanced-react/02-use-reducer/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -95,8 +96,12 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
+
+  // CONTEXT
+  const authCtx = useContext(AuthContext);
+
 
   return (
     <Card className={classes.login}>
--- codes/05-advanced-react/02-use-reducer/src/components/MainHeader/Navigation.js
+++ codes/05-advanced-react/02-use-reducer/src/components/MainHeader/Navigation.js
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import classes from './Navigation.module.css';
+import AuthContext from "../../store/auth-context";
 
-const Navigation = (props) => {
-  return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
-  );
+const Navigation = () => {
+
+    // We have to use useContext() hook to get the context value
+    const ctx = useContext(AuthContext);
+
+    // We have too way to get the context value. The first way is to use useContext() hook
+    // and the second way is to use Consumer component
+    return (
+        // <AuthContext.Consumer>
+        //     {(ctx) => {
+        //         return (
+        <nav className={classes.nav}>
+            <ul>
+                {ctx.isLoggedIn && (
+                    <li>
+                        <a href="/">Users</a>
+                    </li>
+                )}
+                {ctx.isLoggedIn && (
+                    <li>
+                        <a href="/">Admin</a>
+                    </li>
+                )}
+                {ctx.isLoggedIn && (
+                    <li>
+                        <button onClick={ctx.onLogout}>Logout</button>
+                    </li>
+                )}
+            </ul>
+        </nav>
+        //         )
+        //     }
+        //     }
+        //
+        // </AuthContext.Consumer>
+    );
 };
 
 export default Navigation;
