a3e1c0e9de78ea522ccc6948dcf7a5706fd20a3b
Fatih Es <18360859021@ogrenci.btu.edu.tr>
2023-02-16 20:06:39 +0300

    handling http request with redux

---
+++ codes/14-advanced-redux/part-2/src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+
+import store from "./store/index";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
