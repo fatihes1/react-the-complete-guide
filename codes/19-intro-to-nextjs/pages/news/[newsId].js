0098f5c4d95c44c6d20941c7781a384288c9384b
Fatih Es <18360859021@ogrenci.btu.edu.tr>
2023-02-20 10:35:47 +0300

    core features of next | dynamic page | routing with Link from next/link

--- codes/19-intro-to-nextjs/pages/news/[newsId].js
+++ codes/19-intro-to-nextjs/pages/news/[newsId].js
@@ -1,6 +1,6 @@
 // nested paths created file or folder structure unders news
 const DetailPage = () => {
-  return <h1>News Page</h1>;
+  return <h1>Detail Page </h1>;
 };
 
 export default DetailPage;
