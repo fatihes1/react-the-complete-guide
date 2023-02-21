c0bf1c9681369680f17b4b2ac91a70fcc8767b77
Fatih Es <18360859021@ogrenci.btu.edu.tr>
2023-02-21 09:51:13 +0300

    changed the fallback value agaÅin

--- codes/20-intermediate-next/pages/[meetupId]/index.js
+++ codes/20-intermediate-next/pages/[meetupId]/index.js
@@ -32,7 +32,7 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: true,
+    fallback: "blocking",
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
