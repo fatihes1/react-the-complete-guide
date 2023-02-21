65d6c75a466ad1799217c8a231f5897c2fd4a79e
Fatih Es <18360859021@ogrenci.btu.edu.tr>
2023-02-21 16:18:06 +0300

    CSSTransition | Transition | Alternative animation packages

--- codes/21-animating-react-apps/01-starting-project/src/App.js
+++ codes/21-animating-react-apps/01-starting-project/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from "react-transition-group/Transition";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -8,6 +9,7 @@ import List from "./components/List/List";
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: false,
   };
 
   showModal = () => {
@@ -18,12 +20,56 @@ class App extends Component {
     this.setState({ modalIsOpen: false });
   };
 
+  toggleBlock = () => {
+    this.setState((prev) => {
+      return { showBlock: !prev.showBlock };
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        <Backdrop show={this.state.modalIsOpen} />
+        <button
+          className="Button"
+          onClick={this.toggleBlock}
+          style={{ marginBottom: "20px" }}
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={300}
+          mountOnEnter={true}
+          unmountOnExit={true}
+          onEnter={() => console.log("onEnter")}
+          onEntering={() => console.log("onEntering")}
+          onEntered={() => console.log("onEntered")}
+          onExit={() => console.log("onExit")}
+          onExiting={() => console.log("onExiting")}
+          onExited={() => console.log("onExiting")}
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                marginBottom: "20px",
+                transition: "opacity 0.4s ease-out",
+                opacity: state === "exited" ? 0 : 1,
+              }}
+            ></div>
+          )}
+        </Transition>
+
+        <Modal closed={this.closeModal} show={this.state.modalIsOpen} />
+
+        {this.state.modalIsOpen ? (
+          <Backdrop show={this.state.modalIsOpen} />
+        ) : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
--- codes/21-animating-react-apps/01-starting-project/src/components/List/List.js
+++ codes/21-animating-react-apps/01-starting-project/src/components/List/List.js
@@ -1,46 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import TransitionGroup from "react-transition-group/TransitionGroup";
+import CSSTransition from "react-transition-group/CSSTransition";
 
-import './List.css';
+import "./List.css";
 
 class List extends Component {
-    state = {
-        items: [1, 2, 3]
-    }
+  state = {
+    items: [1, 2, 3],
+  };
 
-    addItemHandler = () => {
-        this.setState((prevState) => {
-            return {
-                items: prevState.items.concat(prevState.items.length + 1)
-            };
-        });
-    }
+  addItemHandler = () => {
+    this.setState((prevState) => {
+      return {
+        items: prevState.items.concat(prevState.items.length + 1),
+      };
+    });
+  };
 
-    removeItemHandler = (selIndex) => {
-        this.setState((prevState) => {
-            return {
-                items: prevState.items.filter((item, index) => index !== selIndex)
-            };
-        });
-    }
+  removeItemHandler = (selIndex) => {
+    this.setState((prevState) => {
+      return {
+        items: prevState.items.filter((item, index) => index !== selIndex),
+      };
+    });
+  };
 
-    render () {
-        const listItems = this.state.items.map( (item, index) => (
-            <li 
-                key={index}
-                className="ListItem" 
-                onClick={() => this.removeItemHandler(index)}>{item}</li>
-        ) );
+  render() {
+    const listItems = this.state.items.map((item, index) => (
+      <CSSTransition key={index} classNames="fade" timeout={300}>
+        <li className="ListItem" onClick={() => this.removeItemHandler(index)}>
+          {item}
+        </li>
+      </CSSTransition>
+    ));
 
-        return (
-            <div>
-                <button className="Button" onClick={this.addItemHandler}>Add Item</button>
-                <p>Click Item to Remove.</p>
-                <ul className="List">
-                    {listItems}
-                </ul>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <button className="Button" onClick={this.addItemHandler}>
+          Add Item
+        </button>
+        <p>Click Item to Remove.</p>
+        <TransitionGroup className="List" component="ul">
+          {listItems}
+        </TransitionGroup>
+      </div>
+    );
+  }
 }
 
 export default List;
